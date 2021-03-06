bc682b560d6ad694fae94e8dd04477823b127536
229193-18 <229193-18>
2021-03-06 12:20:37 +0300

    Отправка тестовых текстовых файлов

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Linen;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(320, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Розовый";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Linen;
+            this.button3.Location = new System.Drawing.Point(239, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Серый";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Linen;
+            this.button4.Location = new System.Drawing.Point(158, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Зеленый";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(577, 253);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Цвет";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            RegistryKey myKey = currentUserKey.CreateSubKey("MyKey");
+
+           string color = (string)myKey.GetValue("Color");
+          if (color == "Зеленый")
+                BackColor = System.Drawing.Color.LightGreen;
+          else if (color =="Розовый")
+            BackColor = System.Drawing.Color.LightPink;
+          else if (color =="Серый")
+            BackColor = System.Drawing.Color.Gray;
+        }
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+         
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            RegistryKey myKey = currentUserKey.CreateSubKey("MyKey");
+
+           myKey.SetValue("Color", "Зеленый");
+            myKey.Close();
+            BackColor = System.Drawing.Color.LightGreen;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            RegistryKey myKey = currentUserKey.CreateSubKey("MyKey");
+
+            myKey.SetValue("Color", "Серый");
+            myKey.Close();
+            BackColor = System.Drawing.Color.Gray;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            RegistryKey myKey = currentUserKey.CreateSubKey("MyKey");
+        
+           myKey.SetValue("Color", "Розовый");
+            myKey.Close();
+            BackColor = System.Drawing.Color.LightPink;
+        }
+
+        
+    }
+}
